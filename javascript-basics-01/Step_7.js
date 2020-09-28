510e34e8dcabd39de961aa227be1ee3c1a5443a7
Fatima Al-Batesh <fbatesh@gmail.com>
2020-09-28 11:46:19 +0300

    Step_7 modified

--- javascript-basics-01/Step_7.js
+++ javascript-basics-01/Step_7.js
@@ -1,6 +1,6 @@
 var shoe_size = prompt('Please enter your Shoe Size:');
 var birth_date = prompt('Please enter your Birth Date:');
 function myFunction(shoe_size, birth_date) {
-    alert(shoe_size*2+5)*50-birth_date+1766;   
+    alert((shoe_size*2+5)*50-birth_date+1766);   
   }
   myFunction(shoe_size,birth_date);
