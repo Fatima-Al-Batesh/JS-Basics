442f775167eaf38207fd687924620443ee1ce0a1
Fatima Al-Batesh <fbatesh@gmail.com>
2020-09-30 21:40:07 +0300

    Step_6

--- javascript-basics-02/step 6/Step_6.js
+++ javascript-basics-02/step 6/Step_6.js
@@ -1,8 +1,12 @@
-function func6_1(vara)
-{var newvar1="images/"+vara + "_2.jpg";
-    document.querySelector("#"+vara).src=newvar1;
-}
-function func6_2(_vara)
-{var newvar2="images/"+vara + ".jpg";
-    document.querySelector("#"+vara).src=newvar2;
-}
+var imgs = document.querySelectorAll("img");
+
+Array.from(imgs).forEach((img, i) => {
+    img.addEventListener('mouseover', function () {
+        img.src = "images/image" + (i + 1) + "_2.jpg";
+    })
+});
+Array.from(imgs).forEach((img, i) => {
+    img.addEventListener('mouseout', function () {
+        img.src = "images/image" + (i + 1) + ".jpg";
+    })
+});
